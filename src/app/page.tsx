8f09665bb4e7af706b3006d8f6f7a82f21e9cd18
Gerome <gerome.pm@gmail.com>
2023-10-18 19:11:10 +0800

    Add header component. Refactor hompage code to utilize header component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,51 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import './homepage.css';
+import "./homepage.css";
+import Header from "./components/Header";
 
 export default function Home() {
-  const [loggedUser, setLoggedUser] = useState({});
+  type loggedUser = {
+    id: number;
+    accessToken: string;
+    expiry: string;
+    client: string;
+    uid: string;
+  };
+
+  const [loggedUser, setLoggedUser] = useState({
+    id: 0,
+    accessToken: "",
+    expiry: "",
+    client: "",
+    uid: "",
+  });
   const router = useRouter();
+  const [hasAuthenticatedUser, setHasAuthenticatedUser] = useState(false);
 
   useEffect(() => {
     // console.log(localStorage.getItem("accounts"));
-    //if (!localStorage.getItem("accounts")) router.push("/login");
-    // else setLoggedUser(JSON.parse(localStorage.getItem("accounts")!));
+    if (sessionStorage.getItem("loggedUser")) {
+      setLoggedUser(JSON.parse(sessionStorage.getItem("loggedUser")!));
+      setHasAuthenticatedUser(true);
+    }
   }, []);
 
   return (
     <div className="flex flex-col h-screen items-center">
-      <header className="header">
-        <p>Home </p>
-        <p>About </p>
-        <p>Discover </p>
-        <Link href="/login"><button className="login">Login</button></Link>
-      </header>
-      <main className="main">
+      <Header hasAuthenticatedUser={hasAuthenticatedUser} />
+      <main className="main flex items-center justify-center gap-2 mx-4">
         <div>
-        <h1>Glad to be </h1>
-        <h1>chatting with you!</h1><br />
-        <h3>Chat, share, and express yourself with our all-in-one chatting app.</h3>
-        <h3>Chat with confidence, knowing that your conversations are secure and private.</h3>
+          <h1>Glad to be </h1>
+          <h1>chatting with you!</h1>
+          <br />
+          <h3>
+            Chat, share, and express yourself with our all-in-one chatting app.
+          </h3>
+          <h3>
+            Chat with confidence, knowing that your conversations are secure and
+            private.
+          </h3>
         </div>
         <img src="https://theworkplacecafe.com/wp-content/uploads/2019/07/workplace-illustration-revised-768x362.png"></img>
       </main>
